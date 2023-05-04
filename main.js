5ab707ceb4752b1a5bdc317ee10fee5a9cc22717
davespser <104380606+davespser@users.noreply.github.com>
2023-05-04 16:30:34 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
+function solve(){
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("data").innerHTML = xhttp.responseText;
+        }
+    };
+    xhttp.open("GET", "https://cors-anywhere.herokuapp.com/https://tiny-news-api.herokuapp.com/api/news", true);
+    xhttp.send();
+}
+solve()
+
 document.addEventListener('DOMContentLoaded', () => {
     const elementosCarousel = document.querySelectorAll('.carousel');
      M.Carousel.init(elementosCarousel, {
